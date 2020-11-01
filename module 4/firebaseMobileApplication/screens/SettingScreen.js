31299aadc2049743b56269b34882fc7d3483057f
marina <lotta.marina@outlook.com>
2020-11-01 10:54:55 +0200

    navigations

--- module 4/firebaseMobileApplication/screens/SettingScreen.js
+++ module 4/firebaseMobileApplication/screens/SettingScreen.js
@@ -1,13 +1,9 @@
+// React Navigate Drawer with Bottom Tab – Example using React Navigation V5 //
+// https://aboutreact.com/bottom-tab-view-inside-navigation-drawer //
 import * as React from 'react';
-import {useContext} from 'react';
 import {Button, View, Text, SafeAreaView} from 'react-native';
 
-import {AuthContext} from '../navigation/AuthProvider';
-import FormButton from '../components/FormButton';
-
-const HomeScreen = ({navigation}) => {
-  const {user, logout} = useContext(AuthContext);
-
+const SettingScreen = ({route, navigation}) => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, padding: 16}}>
@@ -23,17 +19,20 @@ const HomeScreen = ({navigation}) => {
               textAlign: 'center',
               marginBottom: 16,
             }}>
-            Home Screen
+            Setting Screen
           </Text>
           <Button
-            onPress={() => navigation.navigate('SettingScreenStack')}
-            title="Go to Setting Screen"
+            onPress={() => navigation.navigate('HomeScreenStack')}
+            title="Go to Home Stack"
+          />
+          <Button
+            onPress={() => navigation.navigate('HomeScreen')}
+            title="Go to Home Screen"
           />
           <Button
             onPress={() => navigation.navigate('ExploreScreen')}
             title="Go to Explore Screen"
           />
-          <FormButton buttonTitle="Logout" onPress={() => logout()} />
         </View>
         <Text style={{fontSize: 18, textAlign: 'center', color: 'grey'}}>
           React Navigate Drawer with Bottom Tab
@@ -46,4 +45,4 @@ const HomeScreen = ({navigation}) => {
   );
 };
 
-export default HomeScreen;
+export default SettingScreen;
