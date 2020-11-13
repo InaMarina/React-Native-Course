bdd6ab871cb03c9aac3205d07ecd123e443a9691
marina <lotta.marina@outlook.com>
2020-11-13 08:52:56 +0200

    addig project

---
+++ module 5-6/ChatApplication/src/screens/RoomScreen.js
@@ -0,0 +1,168 @@
+import React, {useState, useContext, useEffect} from 'react';
+import {
+  GiftedChat,
+  Bubble,
+  Send,
+  SystemMessage,
+} from 'react-native-gifted-chat';
+import {IconButton} from 'react-native-paper';
+import {ActivityIndicator, View, StyleSheet} from 'react-native';
+
+import firestore from '@react-native-firebase/firestore';
+
+import {AuthContext} from '../navigation/AuthProvider';
+
+export default function RoomScreen({route}) {
+  const {user} = useContext(AuthContext);
+  const currentUser = user.toJSON();
+  const {thread} = route.params;
+  const [messages, setMessages] = useState([]);
+
+  // helper method that is sends a message
+  /* function handleSend(newMessage = []) {
+    setMessages(GiftedChat.append(messages, newMessage));
+  } */
+
+  async function handleSend(messages) {
+    const text = messages[0].text;
+
+    firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .add({
+        text,
+        createdAt: new Date().getTime(),
+        user: {
+          _id: currentUser.uid,
+          email: currentUser.email,
+        },
+      });
+
+    await firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .set(
+        {
+          latestMessage: {
+            text,
+            createdAt: new Date().getTime(),
+          },
+        },
+        {merge: true},
+      );
+  }
+
+  useEffect(() => {
+    const messagesListener = firestore()
+      .collection('THREADS')
+      .doc(thread._id)
+      .collection('MESSAGES')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((querySnapshot) => {
+        const messages = querySnapshot.docs.map((doc) => {
+          const firebaseData = doc.data();
+
+          const data = {
+            _id: doc.id,
+            text: '',
+            createdAt: new Date().getTime(),
+            ...firebaseData,
+          };
+
+          if (!firebaseData.system) {
+            data.user = {
+              ...firebaseData.user,
+              name: firebaseData.user.email,
+            };
+          }
+
+          return data;
+        });
+
+        setMessages(messages);
+      });
+
+    // Stop listening for updates whenever the component unmounts
+    return () => messagesListener();
+  }, []);
+
+  function renderBubble(props) {
+    return (
+      // Step 3: return the component
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            // Here is the color change
+            backgroundColor: '#6646ee',
+          },
+        }}
+        textStyle={{
+          right: {
+            color: '#fff',
+          },
+        }}
+      />
+    );
+  }
+  function renderSend(props) {
+    return (
+      <Send {...props}>
+        <View style={styles.sendingContainer}>
+          <IconButton icon="send-circle" size={32} color="#6646ee" />
+        </View>
+      </Send>
+    );
+  }
+  function renderLoading() {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#6646ee" />
+      </View>
+    );
+  }
+  function renderSystemMessage(props) {
+    return (
+      <SystemMessage
+        {...props}
+        wrapperStyle={styles.systemMessageWrapper}
+        textStyle={styles.systemMessageText}
+      />
+    );
+  }
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={handleSend}
+      user={{_id: currentUser.uid}}
+      placeholder="Type your message here..."
+      alwaysShowSend
+      showUserAvatar
+      scrollToBottom
+      renderBubble={renderBubble}
+      renderLoading={renderLoading}
+      renderSend={renderSend}
+      renderSystemMessage={renderSystemMessage}
+    />
+  );
+}
+const styles = StyleSheet.create({
+  sendingContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  systemMessageText: {
+    fontSize: 14,
+    color: '#fff',
+    fontWeight: 'bold',
+    backgroundColor: '#6646ee',
+    padding: 5,
+  },
+});
